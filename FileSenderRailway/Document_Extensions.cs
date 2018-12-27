1e7da5a1cee060694f4faa7b3f20b86a8ddc9586
Andrew <a.a.belousov@uspu.su>
2018-12-27 15:03:57 +0500

    complete classwork

---
+++ FileSenderRailway/Document_Extensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSenderRailway
+{
+    public static class Document_Extensions
+    {
+        public static Document ChangeContent(this Document document, byte[] content)
+            => new Document(document.Name, content, document.Created, document.Format);
+
+    }
+
+    
+}
